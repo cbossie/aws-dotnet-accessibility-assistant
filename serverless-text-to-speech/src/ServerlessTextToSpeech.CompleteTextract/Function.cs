ebadb8fdc1980fe52ebbc77b857f6cf9ca696e90
cbossie <cbossie@gmail.com>
2022-07-26 00:18:20 -0400

    Add callback for textract

---
+++ serverless-text-to-speech/src/ServerlessTextToSpeech.CompleteTextract/Function.cs
@@ -0,0 +1,21 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using ServerlessTextToSpeech.Common;
+
+
+// Boostrap DI Container
+Bootstrap.ConfigureServices();
+
+// The function handler that will be called for each Lambda event
+var handler = (string input, ILambdaContext context) =>
+{
+    return input.ToUpper();
+};
+
+// Build the Lambda runtime client passing in the handler to call for each
+// event and the JSON serializer to use for translating Lambda JSON documents
+// to .NET types.
+await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
+        .Build()
+        .RunAsync();
